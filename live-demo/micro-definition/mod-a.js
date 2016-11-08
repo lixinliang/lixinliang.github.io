9dd2311b77a772a186a2145e1863b521f1d98cb8
李昕亮 <lixinliang@yy.com>
2016-11-08 21:01:29 +0800

    update
    demo

--- live-demo/micro-definition/mod-a.js
+++ live-demo/micro-definition/mod-a.js
@@ -1,4 +1,4 @@
 define('mod-a@1.0.0', ['mod-b', 'mod-c'], function ( b, c ) {
-    console.log('mod-a is definitional/registed, I am waiting for some one use me.');
+    console.log('i am mod-a');
     return 'mod-a,' + b + ',' + c;
 });
